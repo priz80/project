cf94e96f56ca5e41272013f621ff1b84ee74b9bc
Igor <priz8090@gmail.com>
2026-09-01 22:51:12 +0300

    добавил лого

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 let title = "project";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 8;
@@ -5,28 +7,29 @@ let rollback = 10;
 let fullPrice = 101010;
 let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
-screens = screens.toLowerCase();
-console.log(screens);
-console.log(screens.split(", "));
-console.log("Процент отката посреднику за работу" + " " + fullPrice * (rollback / 100));
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
+// console.log(screens.length);
+// console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+// console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+// console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+// screens = screens.toLowerCase();
+// console.log(screens);
+// console.log(screens.split(", "));
+// console.log("Процент отката посреднику за работу" + " " + fullPrice * (rollback / 100));
 
 title = prompt("Как называется ваш проект?");
 screens = prompt("Какие типы экранов нужно разработать? (пример: Простые, Сложные, Интерактивные)");
 screenPrice = prompt("Сколько будет стоить данная работа?");
 adaptive = confirm("Нужен ли адаптив на сайте?");
+
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = prompt("Сколько это будет стоить?");
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
 
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice / 10));
 
@@ -40,4 +43,5 @@ if (fullPrice > 30000) {
     console.log("Что то пошло не так");
 }
 
-console.log(servicePercentPrice);
+console.log("Итого:" + " " + servicePercentPrice + " " + "рублей");
+alert("Итого:" + " " + servicePercentPrice + " " + "рублей");
