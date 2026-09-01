a52f233b9083bb7ac166603cc614b2a4c6424a58
Igor <priz8090@gmail.com>
2026-09-01 16:31:26 +0300

    выполнено задание урока Типы данных, операторы, методы и свойства

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+let title = "project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 8;
+let rollback = 10;
+let fullPrice = 101010;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей");
+screens = screens.toLowerCase();
+console.log(screens);
+console.log(screens.split(", "));
+console.log("Процент отката посреднику за работу" + " " + fullPrice * (rollback / 100));
+
