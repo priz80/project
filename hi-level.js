2edfbdbac4d7c7285e8adea9e125a2a2575a7fdd
Igor <priz8090@gmail.com>
2026-09-04 00:54:51 +0300

    усложненное задание lesson04

--- hi-level.js
+++ hi-level.js
@@ -1,94 +1,24 @@
-let lang = "ru";
-const days = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-const daysEn = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+'use strict'
 
-if (lang === "ru") {
-  console.log("Дни недели на русском:", days);
-} else if (lang === "en") {
-  console.log("Дни недели на английском:", daysEn);
-} else {
-  console.log("Неизвестный язык");
+// let inputArg = 0; // проверка если тип int
+// let inputArg; // проверка если пустая
+let inputArg = "   qwqwqwqwqwasasasasaszxzxzxzxzxq    "; // 31 символ, без учета пропусков до и после строки
+
+
+
+const getArgFunction = (arg) => {
+  let argType = typeof arg;
+  if (argType !== "string") {
+    console.log("это не строка!")
+  } else if (arg === ""){
+    console.log("это пустая строка!")
+  } else {
+    arg = arg.trimStart();
+    arg = arg.trimEnd();
+    if (arg.length > 30)
+      arg = arg.slice(0, 30) + "...";
+    console.log(arg);
+  }
 }
 
-let langs = "ru";
-
-switch (langs) {
-  case "ru":
-    console.log("Дни недели на русском:", [
-      "Понедельник",
-      "Вторник",
-      "Среда",
-      "Четверг",
-      "Пятница",
-      "Суббота",
-      "Воскресенье",
-    ]);
-    break;
-  case "en":
-    console.log("Дни недели на английском:", [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ]);
-    break;
-  default:
-    console.log("Неизвестный язык");
-}
-
-let langd = "ru";
-
-const daysByLang = {
-  ru: [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье",
-  ],
-  en: [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ],
-};
-
-const resultDays = daysByLang[langd] || ["Неизвестный язык"];
-console.log("Дни недели:", resultDays);
-
-let namePerson = "Артем"; // Попробуйте изменить значение
-
-// Логика: если Артем, то директор, иначе (если Александр, то преподаватель, иначе студент)
-let role =
-  namePerson === "Артем"
-    ? "директор"
-    : namePerson === "Александр"
-      ? "преподаватель"
-      : "студент";
-
-console.log(role);
+getArgFunction(inputArg);
