57d439ba75f903b4c3bcec513b382ac6eb2d276b
Igor <priz8090@gmail.com>
2026-09-14 23:06:20 +0300

    lesson17

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello world!')
+'use srtict'
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
+
+
+const second = new Second();
+second.hello();
